c7a0ba8755fe8d762c621da3da116394b280fb08
choi jae won <163416259+wodnjsdl93@users.noreply.github.com>
2024-07-30 15:04:20 +0900

    [feat]player controller,animationData,StateMachine
    플레이어 스크립트 추가

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputs playerInputs { get; private set; }
+    public PlayerInputs.PlayerActions playerActions { get; private set; }
+
+    private void Awake()
+    {
+        playerInputs = new PlayerInputs();
+        playerActions = playerInputs.Player;
+    }
+
+    private void OnEnable()
+    {
+        playerInputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputs.Disable();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Player",menuName = "Characters/Player")]
+
+public class PlayerSO : ScriptableObject
+{
+    [field: SerializeField] public PlayerGroundData GroundData { get; private set; }
+    [field: SerializeField] public PlayerAirData AirData { get; private set; }
+}
