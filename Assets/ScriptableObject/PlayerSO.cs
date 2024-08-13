eb152b3af2e5395d33ce6257ee0fe342c4eca8da
choi jae won <163416259+wodnjsdl93@users.noreply.github.com>
2024-08-13 16:46:12 +0900

    [fix]플레이어 오류버그 해결이 안되서 삭제

--- Assets/ScriptableObject/PlayerSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="Player",menuName = "Characters/Player")]
-
-public class PlayerSO : ScriptableObject
-{
-    [field: SerializeField] public PlayerGroundData GroundData { get; private set; }
-    [field: SerializeField] public PlayerAirData AirData { get; private set; }
-}
