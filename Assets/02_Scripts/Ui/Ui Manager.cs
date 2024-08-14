a282047245fa4cfcc628410c396720f9f1b5a766
HakSoo05 <131574173+HakSoo05@users.noreply.github.com>
2024-08-14 23:29:15 +0900

    {add]Setting 추가

--- Assets/02_Scripts/Ui/Ui Manager.cs
+++ Assets/02_Scripts/Ui/Ui Manager.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 public class UiManager : MonoBehaviour
 {
     public Button startButton;
+    public Button settingButton;
+    public Button noneButton;
 
     void Start()
     {
         startButton.onClick.AddListener(() => OnStartButtonClick());
+        settingButton.onClick.AddListener(() => OnStartButtonClick());
     }
 
     public void OnStartButtonClick()
@@ -16,4 +19,13 @@ public class UiManager : MonoBehaviour
         SceneManager.LoadScene("Enemy");
         SceneManager.LoadScene("Player", LoadSceneMode.Additive);
     }
+    public void OnsettingButtonClick()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+    public void OnNoneButtonClick()
+    {
+        SceneManager.LoadScene("Start");
+    }
 }
+
