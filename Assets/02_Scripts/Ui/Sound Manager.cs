9e1a53eefacc08cf31273597c8e87fe8d4feba82
HakSoo05 <131574173+HakSoo05@users.noreply.github.com>
2024-08-14 18:43:28 +0900

    [UPDATE] 스타트씬 수정

---
+++ Assets/02_Scripts/Ui/Sound Manager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
